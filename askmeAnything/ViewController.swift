c58f8d977f528fc41be74cfebff201db71c0efd8
Morgansimmons25 <68120537+Morgansimmons25@users.noreply.github.com>
2021-07-15 16:59:02 -0700

    8ball

--- askmeAnything/ViewController.swift
+++ askmeAnything/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var ballArray = ["star1","star2","star3", "star4", "star5"]
+var randomballNumber = 0
+    
+    @IBOutlet weak var askimageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newballImage()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askbuttonPress(_ sender: Any) {
+    }
+   
+    func newballImage() {
+        randomballNumber = Int.random(in: 0...4)
+        
+        askimageView.image = UIImage(named: ballArray[randomballNumber])
+    }
 }
 
